e8831b027aaabe36b0bfbcfb2e7343a5ed069e3e
ThiaudioTT <thiago.sky25@gmail.com>
2023-06-01 19:29:19 -0300

    first commit

---
+++ main.clj
@@ -0,0 +1,24 @@
+
+(defn flip-coin
+  "Returns 0 or 1, randomly"
+  []
+  (rand-int 2))
+
+
+(println "How many flips do you want?")
+(def qtdFlips (Integer/parseInt (read-line)))
+
+(println "You said " qtdFlips " flips")
+
+
+(defn flip-till-x
+  "Flips coins x times and return the total result of one of the faces"
+  [x]
+  (if (<= x 0)
+    (flip-coin)
+    (+ (flip-coin) (flip-till-x (dec x)))))
+
+(defn percentageOfFlips [nFaces timesFlipped] (/ nFaces timesFlipped))
+
+
+(println "Your result" (percentageOfFlips (flip-till-x qtdFlips) qtdFlips))
---
+++ readme.md
@@ -0,0 +1,10 @@
+As jogadas devem se manter em 50%, mas quando se joga menos o resultado as vezes eh 20% 19, por ai.
+
+Todo:
+
+Note:
+
+When you flip like 15. Probably 66% of them are head
+when you flip like 1000. Probably 50% of them are head/
+
+Did you see? 50% is the result of 1000 flips. But 66% is the result of 15 flips. So, the more you flip, the more the result will be close to 50%.
